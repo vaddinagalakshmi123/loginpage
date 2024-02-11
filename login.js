00e490cc2331ac8636ec984946306777f5e82154
vaddinagalakshmi123 <159451852+vaddinagalakshmi123@users.noreply.github.com>
2024-02-11 16:05:03 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+function validateLogin(){
+ 
+ 
+ var username = document.getElementById('username').value;
+
+ var password = document.getElementById('password').value;
+
+if(username === 'nagalakshmi@gmail.com' && password === 'password'){
+
+  document.getElementById('errorMessages').textContent = 'login successful';
+
+}else {
+
+  document.getElementById('errorMessages').textContent = 'invalid username or password.please try again..';
+
+}
+
+}
